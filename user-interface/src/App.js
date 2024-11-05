87983ff5b9900273edfc7737e32b1e3dd57988fb
holscherj <holsche2@msu.edu>
2024-11-05 17:37:43 -0500

    Fixing small bug

--- user-interface/src/App.js
+++ user-interface/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Home from "./pages/Home/Home";
 import Pricing from "./pages/Pricing/Pricing";
-import GetStarted from './pages/GetStarted/GetStarted';
 import Contact from "./pages/Contact/Contact";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
